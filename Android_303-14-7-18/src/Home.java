df6b5d816facde5b1bdff4aacdcdff8cec0b2816
shahnawazGold <shahnawazkhan_333@yahoo.com>
2018-07-16 13:47:58 +0500

    first commit

---
+++ Android_303-14-7-18/src/Home.java
@@ -0,0 +1,44 @@
+
+public class Home {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		byte a = 100;
+//       int b =30;
+//       double d1 = 123.4;
+//       boolean one = true;
+//       boolean two = false;
+		
+		//================Arithmetic Operators==========
+//		
+//		   int a = 2;
+//		   int b = 4;
+//		   
+//		   int c = a+b;
+//		   int d = a-b;
+//		   int f = a*b;
+//       
+//		System.out.println("hellow   =  "+c);
+//		System.out.println("hellow   =  "+d);
+//		System.out.println("hellow   =  "+f);
+		
+	//====================Relational Operators================//
+//		 int a = 2;
+//		 int b = 4;
+//		 
+//		 System.out.println("(a ==b)  =  "+(b==a));
+//		 System.out.println("(a ==b)  =  "+(b!=a));
+//		 System.out.println("(a ==b)  =  "+(b >a));
+//		 System.out.println("(a ==b)  =  "+(b <a));
+		 
+		 //================The Logical Operators/============
+		   
+		boolean a =true;
+		boolean b = false ;
+		
+		 System.out.println("(a ==b)  =  "+(a && b)); 
+	}
+
+}
